b1e804f1844b08fb0de3425ba936e8566066e466
dev51312117411 <DyalnLewis411@outlook.com>
2024-07-09 18:39:43 +0200

    error handling

--- hello/greetings/greetings.go
+++ hello/greetings/greetings.go
@@ -1,8 +1,16 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
-func Hello(name string) string {
+func Hello(name string) (string, error) {
+
+	if name == "" {
+		return "", errors.New("Name is required")
+	}
+
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
--- hello/hello.go
+++ hello/hello.go
@@ -2,12 +2,21 @@ package main
 
 import (
 	"fmt"
+	"log"
 
 	"example.com/hello/greetings"
 )
 
 func main() {
-	message := greetings.Hello("Gladys")
+	log.SetPrefix("greetings: ")
+	log.SetFlags(0)
+
+	message, err := greetings.Hello("")
+
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	fmt.Println(message)
+
 }
