54b5af5610c264e6bdcb2ca096905ee2f17969c7
dev51312117411 <DyalnLewis411@outlook.com>
2024-07-09 22:31:23 +0200

    finall for the testing

--- hello/greetings/greetings.go
+++ hello/greetings/greetings.go
@@ -3,6 +3,8 @@ package greetings
 import (
 	"errors"
 	"fmt"
+
+	"math/rand"
 )
 
 func Hello(name string) (string, error) {
@@ -11,6 +13,16 @@ func Hello(name string) (string, error) {
 		return "", errors.New("Name is required")
 	}
 
-	message := fmt.Sprintf("Hi, %v. Welcome!", name)
+	message := fmt.Sprintf(randomname(), name)
 	return message, nil
+}
+
+func randomname() string {
+	format := []string{
+		"Hi %v. Welcome!",
+		"Great to see you, %v",
+		"Hail, %v! well met.",
+	}
+
+	return format[rand.Intn(len(format))]
 }
--- hello/hello.go
+++ hello/hello.go
@@ -11,7 +11,7 @@ func main() {
 	log.SetPrefix("greetings: ")
 	log.SetFlags(0)
 
-	message, err := greetings.Hello("")
+	message, err := greetings.Hello("Dylan")
 
 	if err != nil {
 		log.Fatal(err)
