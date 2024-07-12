5dabb18372b9f92281221bd10f57062e8e87e478
dev51312117411 <DyalnLewis411@outlook.com>
2024-07-12 10:15:24 +0200

    scaning the subscribed blocks with some websocket errors

---
+++ block_subscribe/block_subscribe.go
@@ -0,0 +1,45 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"log"
+
+	"github.com/ethereum/go-ethereum/core/types"
+	"github.com/ethereum/go-ethereum/ethclient"
+)
+
+func main() {
+	client, err := ethclient.Dial("wss://ropsten.infura.io/ws")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("OK")
+	headers := make(chan *types.Header)
+
+	sub, err := client.SubscribeNewHead(context.Background(), headers)
+	if err != nil {
+		log.Fatal(err)
+	}
+	for {
+		select {
+		case err := <-sub.Err():
+			log.Fatal(err)
+		case header := <-headers:
+			fmt.Println(header.Hash().Hex())
+
+			block, err := client.BlockByHash(context.Background(), header.Hash())
+
+			if err != nil {
+				log.Fatal(err)
+			}
+			fmt.Println(block.Hash().Hex())
+			fmt.Println(block.Number().Uint64())
+			fmt.Println(block.Time())
+			fmt.Println(block.Nonce())
+			fmt.Println(len(block.Transactions()))
+		}
+	}
+}
